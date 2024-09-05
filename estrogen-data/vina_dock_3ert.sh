5f588e9fdef461cba9b890968855c5308ae5225f
vinit <vinit21501@iiitd.ac.in>
2024-09-05 12:16:38 +0530

    minor changes.

---
+++ estrogen-data/vina_dock_3ert.sh
@@ -0,0 +1,14 @@
+ iterations=1
+for ligand_path in "$ligands_directory"/*
+do
+    ligand_name=$(basename "$ligand_path" | cut -f1 -d '.')
+    # Create output directory if it doesn't exist
+    mkdir -p "$output_directory/$ligand_name"
+
+    # Create an empty log file
+    echo -e "\n" > "$output_directory/$ligand_name/LOG.txt"
+    for ((i=1; i<=iterations; i++))
+    do
+        ./vina --receptor "$receptor_path" --ligand "$ligand_path" --config "$config_directory" --cpu 36 --exhaustiveness 10 --num_modes 10s --out "$output_directory/$ligand_name/${i}_out.pdbqt" --energy_range 20 >> "$output_directory/$ligand_name/LOG.txt"
+    done
+done
